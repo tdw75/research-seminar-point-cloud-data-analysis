9cb7679f2debffbcda480f4f6d6544343b132a60
tdw75 <tdw75@outlook.com>
2023-08-14 13:25:28 +0200

    add plot cloud function

---
+++ point-clouds/data/visualisation.py
@@ -0,0 +1,15 @@
+from typing import Union
+
+import matplotlib.pyplot as plt
+import numpy as np
+from torch_geometric.data import Data
+
+
+def plot_cloud(cloud: Union[Data, np.ndarray]):
+    if type(cloud, Data):
+        cloud = cloud.pos.numpy()
+    fig = plt.figure(figsize=(5, 5))
+    ax = fig.add_subplot(111, projection="3d")
+    ax.scatter(cloud[:, 0], cloud[:, 1], cloud[:, 2])
+    ax.set_axis_off()
+    plt.show()
